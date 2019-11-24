23eab933732dc052a011bba34c5baf5263c5b66c
Alejandro Gómez <algono@inf.upv.es>
2019-11-24 21:56:06 +0100

    Avances busqueda fnac scraper

--- Entrega2-IEI.Library/Scrapers/FnacScraper.cs
+++ Entrega2-IEI.Library/Scrapers/FnacScraper.cs
@@ -10,22 +10,43 @@ namespace Entrega2_IEI.Library.Scrapers
     public class FnacScraper : IPhoneScraper
     {
         public const string Url = "https://www.fnac.es/";
+        private const string CategoryCssSelector = ".CategoryNav-link", SubCategoryCssSelector = ".categoryMenu-link",
+            SearchInputCssSelector = ".Header__search-input", SearchSubmitCssSelector = ".Header__search-submit";
 
         public IList<Phone> SearchPhone(string brand, string product)
         {
             List<Phone> phones = new List<Phone>();
             using (IWebDriver driver = ScraperUtils.SetupChromeDriver(Url))
             {
-                // TODO: Crear el web scraper para fnac
-                IWebElement categoryList = driver.FindElement(By.CssSelector(".CategoryNav"));
-                IReadOnlyCollection<IWebElement> categories = categoryList.FindElements(By.CssSelector(".CategoryNav-item"));
-                IWebElement phoneCategory = categories.FirstOrDefault(element => element.GetAttribute("content") == "Telefonía y Conectados");
-                phoneCategory.Click();
+                
+                Search(driver, $"Smartphone {brand} {product}");
+
+                // TODO: Extraer datos de los resultados de busqueda
 
                 driver.Quit();
             }
 
             return phones;
+        }
+
+        private static void Search(IWebDriver driver, string text)
+        {
+            IWebElement searchBar = driver.FindElement(By.CssSelector(SearchInputCssSelector));
+            searchBar.SendKeys(text);
+
+            IWebElement searchButton = driver.FindElement(By.CssSelector(SearchSubmitCssSelector));
+            searchButton.Click();
+        }
+
+        private static void GoToCategory(IWebDriver driver, string text) => ClickOnItemFromList(driver, CategoryCssSelector, text);
+
+        private static void GoToSubCategory(IWebDriver driver, string text) => ClickOnItemFromList(driver, SubCategoryCssSelector, text);
+
+        private static void ClickOnItemFromList(IWebDriver driver, string listCssSelector, string text)
+        {
+            IReadOnlyCollection<IWebElement> list = driver.FindElements(By.CssSelector(listCssSelector));
+            IWebElement item = list.FirstOrDefault(element => element.Text == text);
+            item.Click();
         }
     }
 }
