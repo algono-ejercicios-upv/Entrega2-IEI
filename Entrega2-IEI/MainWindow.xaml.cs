ea3c417b73e7fd73256e954ea892fa939c4e9046
Alejandro Gómez <algono@inf.upv.es>
2019-11-25 16:05:46 +0100

    Botón buscar disabled mientras se está buscando (Buscar async) (#1)

--- Entrega2-IEI/MainWindow.xaml.cs
+++ Entrega2-IEI/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Entrega2_IEI.Library;
 using Entrega2_IEI.Library.Scrapers;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -19,11 +21,28 @@ namespace Entrega2_IEI
             CultureInfo.CurrentCulture = new CultureInfo("es-ES");
         }
 
-        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        private async void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
-            BusquedaListBox.Items.Clear();
+            SetBuscando(true);
 
+            IList resultados = null;
             string brand = MarcaBox.Text, model = ModeloBox.Text;
+
+            IList<IPhoneScraper> scrapers = ObtenerScrapers();
+
+            await Task.Run(() =>
+            {
+                resultados = Buscar(brand, model, scrapers);
+            });
+
+            BusquedaListBox.ItemsSource = resultados;
+
+            SetBuscando(false);
+        }
+
+        private IList<IPhoneScraper> ObtenerScrapers()
+        {
+            IList<IPhoneScraper> scrapers = new List<IPhoneScraper>();
 
             foreach (CheckBox box in ScraperBoxes.Children)
             {
@@ -34,14 +53,7 @@ namespace Entrega2_IEI
                         if (typeof(IPhoneScraper).IsAssignableFrom(type))
                         {
                             IPhoneScraper scraper = (IPhoneScraper)Activator.CreateInstance(box.Tag as Type);
-                            BusquedaListBox.Items.Add($"--------- {box.Content} ----------");
-                            
-                            IList<Phone> phones = scraper.SearchPhone(brand, model);
-
-                            foreach (Phone phone in phones)
-                            {
-                                BusquedaListBox.Items.Add(phone);
-                            }
+                            scrapers.Add(scraper);
                         }
                         else
                         {
@@ -58,14 +70,46 @@ namespace Entrega2_IEI
                 }
             }
 
-            if (BusquedaListBox.Items.Count > 0)
+            return scrapers;
+        }
+
+        private IList Buscar(string brand, string model, IEnumerable<IPhoneScraper> scrapers)
+        {
+            IList resultados = new List<object>();
+
+            foreach (IPhoneScraper scraper in scrapers)
+            {
+                // Chapuza hasta poder hacerlo de una forma mejor
+                // TODO: Mejorar esto
+                string webPageName = scraper.GetType().Name;
+                webPageName = webPageName.Remove(webPageName.Length - "Scraper".Length);
+
+                resultados.Add($"--------- {webPageName} ----------");
+
+                IList<Phone> phones = scraper.SearchPhone(brand, model);
+
+                foreach (Phone phone in phones)
+                {
+                    resultados.Add(phone);
+                }
+            }
+
+            if (resultados.Count > 0)
             {
                 const string separator = "---------------------------------------------";
-                BusquedaListBox.Items.Insert(0, separator);
-                BusquedaListBox.Items.Insert(0, "Resultados de la búsqueda:");
+                resultados.Insert(0, separator);
+                resultados.Insert(0, "Resultados de la búsqueda:");
 
-                BusquedaListBox.Items.Add(separator);
+                resultados.Add(separator);
             }
+
+            return resultados;
+        }
+
+        private void SetBuscando(bool buscando)
+        {
+            BuscarButton.IsEnabled = !buscando;
+            BuscandoText.Visibility = buscando ? Visibility.Visible : Visibility.Hidden;
         }
     }
 }
