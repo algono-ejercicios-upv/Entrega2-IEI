04a50b8a3b42abec7faf0226e1f3be6626ff4332
A.Tauste <taustano23@gmail.com>
2019-11-26 18:41:35 +0100

    Added search method in PCComponentesScraper.s

--- Entrega2-IEI.Library/Scrapers/PCComponentesScraper.cs
+++ Entrega2-IEI.Library/Scrapers/PCComponentesScraper.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Entrega2_IEI.Library.Scrapers
 {
@@ -13,14 +14,44 @@ namespace Entrega2_IEI.Library.Scrapers
 
         public override string Url => PCComponentesUrl;
 
+        private const string
+            SearchInputCssSelector = "/html/body/header/div[3]/div[1]/div/div[2]/div/form/input", SearchSubmitCssSelector = "/html/body/header/div[3]/div[1]/div/div[2]/div/form/input",
+            ArticleItemCssSelector = "tarjeta-articulo expandible", ArticleDescriptionCssSelector = "tarjeta-articulo__nombre",
+            ArticleOldPriceCssSelector = "tarjeta-articulo__pvp", ArticlePriceCssSelector = "tarjeta-articulo__precio-actual";
+
         public override IEnumerable<Phone> SearchPhone(IWebDriver driver, string brand, string model)
         {
-            // Descomentar las líneas de código cuando vayas a escribir el código (y quites la NotImplementedException)
+            List<Phone> phones = new List<Phone>();
 
-            // TODO: Crear el web scraper para PCComponentes
-            throw new NotImplementedException();
+            Search(driver, $"Smartphone {brand} {model}");
 
-            //driver.Quit();
+            IReadOnlyCollection<IWebElement> articleList = GetArticleItemList(driver);
+
+            foreach (IWebElement artitleItem in articleList)
+            {
+                Phone phone = null;
+                try 
+                {
+                    // Obtener el telefono (si existe)
+                    // Si no encontrara algún elemento, saltaría al catch e iría al siguiente
+                }
+                catch (NoSuchElementException ex)
+                {
+                    Debug.WriteLine("Skipping article: " + ex.Message);
+                }
+
+                if (phone != null) yield return phone;
+            }
+
+            driver.Quit();
+        }
+
+        private static IReadOnlyCollection<IWebElement> GetArticleItemList(IWebDriver driver) => driver.FindElements(By.CssSelector(ArticleItemCssSelector));
+
+        private static void Search(IWebDriver driver, string v)
+        {
+            IWebElement searchBar = driver.FindElement(By.XPath(SearchInputCssSelector));
+            searchBar.SendKeys(v);
         }
     }
 }
